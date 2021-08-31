919ee9ca0fb5ad9b547e9dcec5cbdaed404f9f6d
Radhika S <radhika.s@tvsnext.io>
2021-08-31 15:21:00 +0530

    [Radhika]  Add: Added a style cop for manager project

--- Manager/Interface/IUserManager.cs
+++ Manager/Interface/IUserManager.cs
@@ -1,15 +1,43 @@
-﻿using FundooNotes.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IUserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Radhika"/>
+// ----------------------------------------------------------------------------------------------------------
 
 namespace FundooNotes.Managers.Interface
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FundooNotes.Models;
+
+    /// <summary>
+    /// interface for user manager class
+    /// </summary>
     public interface IUserManager
     {
+        /// <summary>
+        /// definition for register method
+        /// </summary>
+        /// <param name="userData">passing a register model</param>
+        /// <returns>returning as boolean value</returns>
         bool Register(RegisterModel userData);
+
+        /// <summary>
+        /// definition for login method
+        /// </summary>
+        /// <param name="email">email as string</param>
+        /// <param name="password">[password as string</param>
+        /// <returns>boolean value</returns>
         bool Login(string email, string password);
-        bool ForgetPassword(string Email);
+
+        /// <summary>
+        /// definition for forgot password
+        /// </summary>
+        /// <param name="email">email as string type</param>
+        /// <returns>boolean value</returns>
+        bool ForgetPassword(string email);
     }
 }
--- Manager/Manager/UserManager.cs
+++ Manager/Manager/UserManager.cs
@@ -1,50 +1,85 @@
-﻿using FundooNotes.Managers.Interface;
-using FundooNotes.Models;
-using FundooNotes.Repository.Interface;
-using Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Radhika"/>
+// ----------------------------------------------------------------------------------------------------------
 
 namespace FundooNotes.Managers.Manager
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FundooNotes.Managers.Interface;
+    using FundooNotes.Models;
+    using FundooNotes.Repository.Interface;
+
+   /// <summary>
+   /// Class user manager
+   /// </summary>
     public class UserManager : IUserManager
     {
+        /// <summary>
+        /// declaring repository
+        /// </summary>
         private readonly IUserRepository repoistory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserManager"/> class
+        /// </summary>
+        /// <param name="repoistory">repository as parameter</param>
         public UserManager(IUserRepository repoistory)
         {
             this.repoistory = repoistory;
         }
+
+        /// <summary>
+        /// register method for manager class
+        /// </summary>
+        /// <param name="userData">passing register model data</param>
+        /// <returns>true or false(boolean value)</returns>
         public bool Register(RegisterModel userData)
         {
             try
             {
                 return this.repoistory.Register(userData);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
         }
-        public bool Login(string email,string password)
+
+        /// <summary>
+        /// login method 
+        /// </summary>
+        /// <param name="email">email as string</param>
+        /// <param name="password">password as string</param>
+        /// <returns>boolean value</returns>
+        public bool Login(string email, string password)
         {
             try
             {
-                return this.repoistory.Login(email,password);
+                return this.repoistory.Login(email, password);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
         }
-        public bool ForgetPassword(string Email)
+
+        /// <summary>
+        /// forgot password method
+        /// </summary>
+        /// <param name="email">email as string</param>
+        /// <returns>return true for repository</returns>
+        public bool ForgetPassword(string email)
         {
             try
             {
-                //Send userdata to Repository and return result true or false
-                return this.repoistory.ForgetPassword(Email);
+                // Send userdata to Repository and return result true or false
+                return this.repoistory.ForgetPassword(email);
             }
             catch (Exception ex)
             {
