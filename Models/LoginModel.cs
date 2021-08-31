deabaad6b69ef1ad9857449e9af45a9a6b0b2ca2
Radhika S <radhika.s@tvsnext.io>
2021-08-31 14:54:34 +0530

    [Radhika] Add:Added Style cop for model project

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoginModel.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Radhika"/>
+// ----------------------------------------------------------------------------------------------------------
 
 namespace Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// Login model class
+    /// </summary>
     public class LoginModel
     {
+        /// <summary>
+        /// Gets or sets email id and it is required field
+        /// </summary>
         [Required]
         public string EmailId { get; set; }
 
+        /// <summary>
+        /// Gets or sets password and it is required field
+        /// </summary>
         [Required]
         public string Password { get; set; }
     }
--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RegisterModel.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Radhika"/>
+// ----------------------------------------------------------------------------------------------------------
 
 namespace FundooNotes.Models
 {
@@ -7,16 +12,39 @@ namespace FundooNotes.Models
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
+
+    /// <summary>
+    /// register model data class
+    /// </summary>
     public class RegisterModel
     {
+        /// <summary>
+        /// Gets or sets the user id(primary key) 
+        /// </summary>
         [Key]
         public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name
+        /// </summary>
         [Required]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name
+        /// </summary>
         [Required]
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email
+        /// </summary>
         [Required]
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password
+        /// </summary>
         [Required]
         public string Password { get; set; }
     }
